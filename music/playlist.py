25ee99c97ae5f9b2990da68888c1838830eac9a0
Piop2 <seungyounyou0220@gmail.com>
2023-05-05 10:08:35 +0900

    update playlist

--- music/playlist.py
+++ music/playlist.py
@@ -3,6 +3,12 @@ from dataclasses import dataclass
 from music.exceptions import QueueIsEmpty
 
 
+@dataclass()
+class Music:
+    title: str
+    author: str
+
+
 class PlayList:
     def __init__(self):
         self.queue: list[dict[str: str]] = []
@@ -12,7 +18,12 @@ class PlayList:
             return False
         return True
 
-    def current_music(self) -> None:
+    def is_next_exist(self) -> bool:
+        if len(self.queue) <= 1:
+            return False
+        return True
+
+    def current_music(self) -> Music:
         if self.is_empty():
             raise QueueIsEmpty()
         return self.queue[0]
@@ -20,8 +31,15 @@ class PlayList:
     def add(self, value):
         self.queue.append(value)
 
+    def next(self) -> Music:
+        self.queue.pop(0)
+        return self.queue[0]
 
-@dataclass()
-class Music:
-    title: str
-    author: str
+    def jump_to(self, value: int) -> Music:
+        self.queue = self.queue[value:]
+        return self.queue[0]
+
+    def __getitem__(self, item: int) -> Music | list[Music]:
+        if self.is_empty():
+            raise QueueIsEmpty
+        return self.queue[item]
