f09218f5ce579cc3864f6a58775ba6f45921640f
biru3 <49255122+biru3@users.noreply.github.com>
2023-05-13 18:05:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from bot import ChioMusic
 
 if __name__ == "__main__":
     load_dotenv()
-    discord_api_token = os.environ.get("DiscordApiToken")
+    token = os.environ.get("DiscordApiToken")
 
-    ChioMusic().run(token=discord_api_token)
+    ChioMusic().run(token)
